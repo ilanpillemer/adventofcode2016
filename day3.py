5e96f37591c86fd3ff36b2f273ecd2861cf3a126
Ilan Pillemer <ilan.pillemer@gmail.com>
2018-01-03 17:17:21 +0000

    strip() is not required before using int(_)

--- day3.py
+++ day3.py
@@ -11,7 +11,7 @@ def howManyValid(f):
     count = 0
     for line in open(f):
         numbers = line.split()
-        a,b,c = int(numbers[0].strip()),int(numbers[1].strip()),int(numbers[2].strip())
+        a,b,c = int(numbers[0]),int(numbers[1]),int(numbers[2])
         if valid(a,b,c) : count = count + 1
     print("valid row wise triangles:{}".format(count))
 
@@ -20,7 +20,7 @@ def howManyValidColumnWise(f):
     line_no = 1
     for line in open(f):
         numbers = line.split()
-        a,b,c = int(numbers[0].strip()),int(numbers[1].strip()),int(numbers[2].strip())
+        a,b,c = int(numbers[0]),int(numbers[1]),int(numbers[2])
         if line_no == 1 : a1,b1,c1 = a,b,c
         elif line_no == 2: a2,b2,c2 = a,b,c
         elif line_no == 3: a3,b3,c3 = a,b,c
