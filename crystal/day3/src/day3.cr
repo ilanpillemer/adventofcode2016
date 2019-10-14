f1f6bf8c3927423c6cde9b477de4b30a26475148
Ilan <ilan.pillemer@gmail.com>
2019-10-14 21:53:42 +0100

    crystal day 3

--- crystal/day3/src/day3.cr
+++ crystal/day3/src/day3.cr
@@ -1,6 +1,13 @@
-# TODO: Write documentation for `Day3`
-module Day3
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
+def valid(arr : Array(Int32))
+  arr.each_permutation do |i|
+    return false if i[0] + i[1] <= i[2]
+  end
+  true
 end
+
+puts "Advent of Code 2016, Day 3"
+count = STDIN.each_line.count do |line|
+  valid(line.split.map(&.to_i))
+end
+
+puts("part1 => #{count} valid triangles")
---
+++ crystal/day3/src/day3_p2.cr
@@ -0,0 +1,33 @@
+def valid(arr : Array(Int32))
+  arr.each_permutation do |i|
+    return false if i[0] + i[1] <= i[2]
+  end
+  true
+end
+
+count = 0
+t1 = [] of Int32
+t2 = [] of Int32
+t3 = [] of Int32
+
+STDIN.each_line do |l|
+  arr = l.split.map(&.to_i)
+
+  t1 << arr[0]
+  t2 << arr[1]
+  t3 << arr[2]
+end
+
+count += t1.in_groups_of(3, 0).count do |a|
+  valid(a)
+end
+
+count += t2.in_groups_of(3, 0).count do |a|
+  valid(a)
+end
+
+count += t3.in_groups_of(3, 0).count do |a|
+  valid(a)
+end
+
+puts("part1 => #{count} valid triangles")
