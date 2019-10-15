85dce5c8e04f7f769d40604548b82ab684466433
Ilan <ilan.pillemer@gmail.com>
2019-10-15 22:13:11 +0100

    crystal day 6 part 1

---
+++ crystal/day6/day6.cr
@@ -0,0 +1,6 @@
+answer = [] of Char | Nil
+STDIN.each_char.select(&.!=('\n')).to_a.in_groups_of(8).transpose.each do |l|
+  puts(l)
+  answer << l.tally.max_by { |k, v| v }.first
+end
+puts answer.join
