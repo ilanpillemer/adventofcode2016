163294ea4ca53947da0d79b929769a588693d9e1
Ilan <ilan.pillemer@gmail.com>
2019-11-01 19:29:49 +0000

    crystal day 14 part 1

---
+++ crystal/day14/day14.cr
@@ -0,0 +1,39 @@
+require "digest/md5"
+
+class KeyPad
+  property sevens : Set(String)
+
+  def initialize
+    @sevens = Set(String).new
+  end
+
+  def key(h)
+    return true if valid(h)
+  end
+
+  def valid(prefix, i)
+    h = Digest::MD5.hexdigest("#{prefix}#{i}")
+    #    puts "#{prefix}#{i}"
+    if /(.)\1{2}/ =~ h
+      c = $1.to_s
+      return true if @sevens.includes?(c + ":" + h)
+      1.upto(1000) do |j|
+        h2 = Digest::MD5.hexdigest("#{prefix}#{i + j}")
+        if /#{c}{5}/ =~ h2
+          @sevens << (c + ":" + h)
+          return true
+        end
+      end
+    end
+  end
+end
+
+k = KeyPad.new
+count = 0
+i = -1
+until count == 64
+  i += 1
+  count += 1 if k.valid("jlmsuwbz", i)
+end
+
+puts i
