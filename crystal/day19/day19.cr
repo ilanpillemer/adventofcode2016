efbe04bb12279ddf57279eff53fc06185b9ffdeb
Ilan <ilan.pillemer@gmail.com>
2019-11-05 19:08:10 +0000

    crystal day 19 part 2

--- crystal/day19/day19.cr
+++ crystal/day19/day19.cr
@@ -9,6 +9,14 @@ module Party
 
     def initialize(@id, @presents, @prev = self, @next = self)
     end
+
+    def hop(count)
+      opp = self.dup
+      1.upto(count) do
+        opp = opp.next
+      end
+      opp
+    end
   end
 
   def self.setup(num)
@@ -33,16 +41,37 @@ module Party
     elves[1]
   end
 
-  def self.play(e : Elf)
-    elf = e
+  def self.play(elf : Elf)
     until elf.next.id == elf.id
       elf.next = elf.next.next
       elf = elf.next
     end
     elf
   end
+
+  def self.play2(elf : Elf, total : Int32)
+    until elf.next.id == elf.id
+      opp = elf.hop(total//2)
+      total -= 1
+      opp.prev.next = opp.next
+      opp.next.prev = opp.prev
+      elf = elf.next
+      exit if total == 0
+    end
+    elf
+  end
 end
 
-pp Party.play(Party.setup(5)).id
 puts "part 1"
-pp Party.play(Party.setup(3005290)).id
+# pp Party.play(Party.setup(5)).id
+# pp Party.play(Party.setup(3005290)).id
+
+puts "part 2"
+
+2.upto(100) do |i|
+  printf("%d \t->\t %d\n", i, Party.play2(Party.setup(i), i).id)
+end
+# pp Party.play2(Party.setup(3005290), 3005290).id
+# cycle repeats 3^a + 1
+
+pp 3005290 - 1594323
